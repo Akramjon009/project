a9d0175b91adcc6a6b836301758f509baa83fc13
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-04-04 12:24:11 +0500

    home work

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -1,14 +1,36 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild,inject} from '@angular/core';
 import { NgbCarousel, NgbCarouselModule, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { NgbCalendar, NgbDate, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgbCarouselModule, FormsModule,NgbRatingModule],
+  imports: [NgbCarouselModule, FormsModule,NgbRatingModule,NgbDatepickerModule, JsonPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  styles: `
+		.custom-day {
+			text-align: center;
+			padding: 0.185rem 0.25rem;
+			display: inline-block;
+			height: 2rem;
+			width: 2rem;
+		}
+		.custom-day.focused {
+			background-color: #e6e6e6;
+		}
+		.custom-day.range,
+		.custom-day:hover {
+			background-color: rgb(2, 117, 216);
+			color: white;
+		}
+		.custom-day.faded {
+			background-color: rgba(2, 117, 216, 0.5);
+		}
+	`,
 })
 export class AppComponent {
   title = 'project';
@@ -46,4 +68,40 @@ export class AppComponent {
 	selected = 0;
 	hovered = 0;
 	readonly = false;
+
+	calendar = inject(NgbCalendar);
+
+	hoveredDate: NgbDate | null = null;
+	fromDate: NgbDate = this.calendar.getToday();
+	toDate: NgbDate | null = this.calendar.getNext(this.fromDate, 'd', 10);
+
+	onDateSelection(date: NgbDate) {
+		if (!this.fromDate && !this.toDate) {
+			this.fromDate = date;
+		} else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
+			this.toDate = date;
+		} else {
+			this.toDate = null;
+			this.fromDate = date;
+		}
+	}
+
+	isHovered(date: NgbDate) {
+		return (
+			this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate)
+		);
+	}
+
+	isInside(date: NgbDate) {
+		return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
+	}
+
+	isRange(date: NgbDate) {
+		return (
+			date.equals(this.fromDate) ||
+			(this.toDate && date.equals(this.toDate)) ||
+			this.isInside(date) ||
+			this.isHovered(date)
+		);
+	}
 }
