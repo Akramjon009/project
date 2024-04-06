00362eadbabd3c4918d4f73f06484b1d879f00e6
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-04-06 09:16:48 +0500

    commite

---
+++ projects/src/app/cars/cars.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cars',
+  standalone: true,
+  imports: [],
+  templateUrl: './cars.component.html',
+  styleUrl: './cars.component.scss'
+})
+export class CarsComponent {
+
+}
