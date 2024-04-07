c1b514ab162fd80937e51ea3c985d5d1df46e20f
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-04-07 10:47:09 +0500

    home work

--- leetcodeExampleTwo/README.md
+++ leetcodeExampleTwo/README.md
@@ -1,4 +1,4 @@
-# Leetcode
+# LeetcodeExampleTwo
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.3.
 
--- leetcodeExampleTwo/src/app/app.component.ts
+++ leetcodeExampleTwo/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Leetcode';
+  title = 'leetcodeExampleTwo';
+  
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
