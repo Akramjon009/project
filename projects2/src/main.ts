7dd6ccd0e62b06050f3d6ae469a603f23b9754c8
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-04-15 11:44:07 +0500

    update

--- projects2/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/AppModule';
-
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
